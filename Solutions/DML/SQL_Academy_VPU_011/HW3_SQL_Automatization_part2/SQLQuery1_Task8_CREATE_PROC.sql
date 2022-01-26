250447be305e9dc4b77f7154b7fcc5cefa66f7c4
JdS161 <a.vasiltschenko@gmail.com>
2022-01-26 20:11:07 +0300

    Addition of the procedure creation query from previous HW in order the current HW being consistent

--- Solutions/DML/SQL_Academy_VPU_011/HW3_SQL_Automatization_part2/SQLQuery1_Task8_CREATE_PROC.sql
+++ Solutions/DML/SQL_Academy_VPU_011/HW3_SQL_Automatization_part2/SQLQuery1_Task8_CREATE_PROC.sql
@@ -8,11 +8,9 @@ GO
 				--1) First date&time 
 				--2) Duration of the discipline
 				--3) Location
-				--4) Group
-				--5) Discipline
-				--6) Teacher
-				--7) Topic (optional)
-
+				--4) Discipline
+				--5) Teacher
+				--6) Topic (optional)
 CREATE PROC sp_scheduleInsert 
 							@varDateTime datetime, 
 							@varDuration int, 
@@ -49,5 +47,5 @@ END
 GO
 
 --Run next query when procedure no longer is required
-DROP PROC sp_scheduleInsert
-GO
+--DROP PROC sp_scheduleInsert
+--GO
